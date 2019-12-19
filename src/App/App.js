20cf310ba909870f3506c24a1d7c8c389e10a44b
alijany <mh.alijany@gamil.com>
2019-12-19 20:24:03 +0330

    add getway input to app

--- src/App/App.js
+++ src/App/App.js
@@ -22,13 +22,27 @@ const App = () => {
                 <div className="col-1 text-center p-0">
                     <span>to</span>
                 </div>
-                
+
                 <div className="col pr-0">
                     <input type="number" className="form-control" placeholder="Private port" />
                     {/* <small id="emailHelp" className="form-text text-muted">Errors ... </small> */}
                 </div>
             </div>
 
+            <div className="row pb-5  mx-3">
+                <div className="col-12 p-0 mb-2">
+                    <div className="custom-control custom-checkbox mb-2">
+                        <input type="checkbox" className="custom-control-input" id="Auto" />
+                        <label className="custom-control-label" htmlFor="Auto">Set Gateway Auto</label>
+                    </div>
+                </div>
+
+                <div className="col-12 p-0">
+                    <input type="text" className="form-control" placeholder="example : 192.168.1.1" />
+                    {/* <small id="emailHelp" className="form-text text-muted">Errors ... </small> */}
+                </div>
+            </div>
+            
         </div>
     );
 }
